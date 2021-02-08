961e4ea5de1345acaa97a19942468f29d36b5742
RigelPrimus <barbu.razvan@icloud.com>
2021-02-08 13:02:36 +0200

    test

---
+++ Projects/Git/lala..py
@@ -0,0 +1,4 @@
+ git config --global credential.helper store
+ git config --global user.name "<RigelPrimus>"
+ git config --global user.email "<barbu.razvan@icloud.com>"
+ git config --global user.password "<Stansibran1233>"
